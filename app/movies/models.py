a9ded7fe5fab20b38e0ab5f9730e63db90b25011
“Keith <>
2021-06-11 12:10:52 -0500

    initial code upload

---
+++ app/movies/models.py
@@ -0,0 +1,10 @@
+
+# Create your models here.
+# app/movies/models.py
+
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    pass
